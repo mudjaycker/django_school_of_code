bc285897fb60074f3100bd32e4877d858ebc6810
mudjaycker <tombutoyi@gmail.com>
2022-09-06 11:46:34 +0200

    add Model todo

--- todo_list/todo_app/models.py
+++ todo_list/todo_app/models.py
@@ -9,3 +9,12 @@ def apres_une_semaine():
 
 class Auteur(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    num_phone = models.CharField(max_length=10, unique=True)
+
+
+class ToDo(models.Model):
+    auteur = models.ForeignKey(Auteur, to_field="user" ,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=apres_une_semaine)
