034e81602471a2342254924dfe3247b974085823
mudjaycker <tombutoyi@gmail.com>
2022-09-06 11:39:22 +0200

    first migrations [creating table auteur]

--- todo_list/todo_app/models.py
+++ todo_list/todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
+
+def apres_une_semaine():
+    return timezone.now() + timezone.timedelta(days=7)
 # Create your models here.
+
+class Auteur(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
