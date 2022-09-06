8161136d230b59406a537eab6c126f4920961ac3
mudjaycker <tombutoyi@gmail.com>
2022-09-06 13:07:32 +0200

    first Saving from template

--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -5,10 +5,11 @@ from .models import Auteur, ToDo
 
 def add_new_todo(request):
     if request.method == "POST":
-        auteur = request.POST.get("auteur")
+        auteur_id = request.POST.get("auteur")
+        auteur_instance = Auteur.objects.get(user=auteur_id)
         titre = request.POST.get("titre")
         description = request.POST.get("description")
-        date_fin = request.POST.get("date_fin")
-        print(f"auteur = {auteur}, titre = {titre},  description = {description}, date_fin = {date_fin}")
-        print("datas = ", request.POST)
+        date_fin = request.POST.get("date-fin")
+        todo = ToDo(auteur=auteur_instance, titre=titre, description=description, date_fin=date_fin)
+        todo.save()
     return render(request, template_name="add_todo.html")
