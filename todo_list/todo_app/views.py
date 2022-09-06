76b309218db223f6947c9548c48e563929aaf7ed
mudjaycker <tombutoyi@gmail.com>
2022-09-06 12:55:39 +0200

    adding csrf_token and verifiying if data are read in the server (view)

--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -4,4 +4,11 @@ from .models import Auteur, ToDo
 # Create your views here.
 
 def add_new_todo(request):
-    return render(request, template_name="add_todo.html", context=locals())
+    if request.method == "POST":
+        auteur = request.POST.get("auteur")
+        titre = request.POST.get("titre")
+        description = request.POST.get("description")
+        date_fin = request.POST.get("date_fin")
+        print(f"auteur = {auteur}, titre = {titre},  description = {description}, date_fin = {date_fin}")
+        print("datas = ", request.POST)
+    return render(request, template_name="add_todo.html")
