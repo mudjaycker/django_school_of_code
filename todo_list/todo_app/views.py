df383a26a2f87b655786ba3a9c52648ebe196310
mudjaycker <tombutoyi@gmail.com>
2022-09-06 12:01:42 +0200

    init view [add_todo]

--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Auteur, ToDo
 
 # Create your views here.
+
+def add_new_todo(request):
+    return render(request, template_name="add_todo.html", context=locals())
