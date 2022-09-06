d6d1df1bc2c2a0e8c232d878c686281ba5e91dcb
mudjaycker <tombutoyi@gmail.com>
2022-09-06 17:49:22 +0200

    name in path urls

--- todo_list/todo_app/urls.py
+++ todo_list/todo_app/urls.py
@@ -3,11 +3,11 @@ from . views import *
 
 
 urlpatterns = [
-    path('add-todo/', add_new_todo),
-    path('list-todos/', list_todos),
-    path('one-task/<int:pk>/', get_one_task),
-    path('delete-task/<int:pk>/', delete_task),
-    path('update-task/<int:pk>/', update_task),
-    path('login/', login),
-    path('enregistrement/', enregistrement),
+    path('add-todo/', add_new_todo, name='add_todo'),
+    path('list-todos/', list_todos, name="list_todo"),
+    path('one-task/<int:pk>/', get_one_task, name="one-task"),
+    path('delete-task/<int:pk>/', delete_task, name="delete_task"),
+    path('update-task/<int:pk>/', update_task, name="update_task"),
+    path('login/', login, name="login"),
+    path('enregistrement/', enregistrement, name="enregistrement"),
 ]
